f113af1dd051ae83badbf4744b2e23d2e8983d46
la236500-Benadis-K <la236500@student.helha.be>
2026-05-07 20:30:05 +0200

    ajout des tests unitaire pour InitialiserJeu()

--- TestJeu/Test3Calculs.cs
+++ TestJeu/Test3Calculs.cs
@@ -66,9 +66,9 @@ public class TestCalcul
         Snake.CaseDeJeu corps = partie.Serpent[1];
         Snake.CaseDeJeu queue = partie.Serpent[2];
 
-        Assert.AreEqual(2,tete.y);
-        Assert.AreEqual(1,corps.y);
-        Assert.AreEqual(0,queue.y);
+        Assert.AreEqual(2, tete.y);
+        Assert.AreEqual(1, corps.y);
+        Assert.AreEqual(0, queue.y);
         Assert.AreEqual(tete.x, queue.x);
     }
 
@@ -114,6 +114,43 @@ public class TestCalcul
         Assert.AreEqual(1, corps.x);
         Assert.AreEqual(0, queue.x);
         Assert.AreEqual(tete.y, queue.y);
+    }
+    /**************************************************
+        Tests InitialiserJeu()
+    **************************************************/
+
+    /// <summary>
+    /// On vérifie que la culture est correcte
+    /// </summary>
+    [TestMethod]
+    public void TestInitialiserJeuReturnPartieEtChangeCulture()
+    {
+
+
+        // Act
+        Snake.Partie partie = Snake.InitialiserJeu();
+
+        // Assert
+        Assert.IsNotNull(partie);
+        Assert.AreEqual("fr-BE", System.Globalization.CultureInfo.CurrentCulture.Name);
+    }
+
+    /// <summary>
+    /// On vérifie que les configurations par défaut sont chargées
+    /// </summary>
+    [TestMethod]
+    public void TestInitialiserJeuChargeConfigurationParDefaut()
+    {
+        // Arrange
+        Snake.LARGEUR_TERRAIN = 20;
+        Snake.COULEUR_BORD = ConsoleColor.Yellow;
+
+        // Act
+        Snake.InitialiserJeu();
+
+        // Assert
+        Assert.AreEqual(30, Snake.LARGEUR_TERRAIN);
+        Assert.AreEqual(ConsoleColor.White, Snake.COULEUR_BORD);
     }
 
     [TestMethod]
@@ -326,85 +363,85 @@ public class TestCalcul
     [TestMethod]
     public void AvancerSerpentCollisionMurHaut()
     {
-        
+
         Snake.Partie partie = new();
         partie.Serpent = new List<Snake.CaseDeJeu>();
-        
+
         //on place le serpent avec la tete qui regarde vers le haut
-        partie.Serpent.Add(new Snake.CaseDeJeu(5,0)); 
+        partie.Serpent.Add(new Snake.CaseDeJeu(5, 0));
         partie.Serpent.Add(new Snake.CaseDeJeu(5, 1));
         partie.Serpent.Add(new Snake.CaseDeJeu(5, 2));
-        
+
         partie.PartieEnCours = true;
         partie.DirectionSerpent = Snake.Directions.Haut;//on le fais avancer vers le haut 
-        
+
         partie.PositionGateau = new Snake.CaseDeJeu(10, 5);
-        
+
         Snake.AvancerSerpent(ref partie);
-        
+
         Assert.IsFalse(partie.PartieEnCours);// il cogne le bord donc le jeu s'arrête
     }
-    
+
     [TestMethod]
     public void AvancerSerpentCollisionMurBas()
     {
         Snake.Partie partie = new();
         partie.Serpent = new List<Snake.CaseDeJeu>();
-        
+
         //serpent tout en bas
-        partie.Serpent.Add(new Snake.CaseDeJeu(5, Snake.HAUTEUR_TERRAIN-1)); 
-        partie.Serpent.Add(new Snake.CaseDeJeu(5, Snake.HAUTEUR_TERRAIN-2));
-        partie.Serpent.Add(new Snake.CaseDeJeu(5, Snake.HAUTEUR_TERRAIN-3));
-        
+        partie.Serpent.Add(new Snake.CaseDeJeu(5, Snake.HAUTEUR_TERRAIN - 1));
+        partie.Serpent.Add(new Snake.CaseDeJeu(5, Snake.HAUTEUR_TERRAIN - 2));
+        partie.Serpent.Add(new Snake.CaseDeJeu(5, Snake.HAUTEUR_TERRAIN - 3));
+
         partie.PartieEnCours = true;
         partie.DirectionSerpent = Snake.Directions.Bas; // va vers le bas
-        
+
         partie.PositionGateau = new Snake.CaseDeJeu(10, 5);
-        
+
         Snake.AvancerSerpent(ref partie);
-        
+
         Assert.IsFalse(partie.PartieEnCours);//le jeu s'arrête quand il cogne
     }
-    
+
     [TestMethod]
     public void AvancerSerpentCollisionMurDroite()
     {
         Snake.Partie partie = new();
         partie.Serpent = new List<Snake.CaseDeJeu>();
-        
+
         //serpent tout à droite
-        partie.Serpent.Add(new Snake.CaseDeJeu(Snake.LARGEUR_TERRAIN-1,2)); 
-        partie.Serpent.Add(new Snake.CaseDeJeu(Snake.LARGEUR_TERRAIN-2,2));
-        partie.Serpent.Add(new Snake.CaseDeJeu(Snake.LARGEUR_TERRAIN-3,2));
-        
+        partie.Serpent.Add(new Snake.CaseDeJeu(Snake.LARGEUR_TERRAIN - 1, 2));
+        partie.Serpent.Add(new Snake.CaseDeJeu(Snake.LARGEUR_TERRAIN - 2, 2));
+        partie.Serpent.Add(new Snake.CaseDeJeu(Snake.LARGEUR_TERRAIN - 3, 2));
+
         partie.PartieEnCours = true;
         partie.DirectionSerpent = Snake.Directions.Droite; //va vers la droite
-        
+
         partie.PositionGateau = new Snake.CaseDeJeu(10, 5);
-        
+
         Snake.AvancerSerpent(ref partie);
-        
+
         Assert.IsFalse(partie.PartieEnCours);//cogne le bord donc le jeu s'arrête
     }
-    
+
     [TestMethod]
     public void AvancerSerpentCollisionMurGauche()
     {
         Snake.Partie partie = new();
         partie.Serpent = new List<Snake.CaseDeJeu>();
-        
+
         //serpent tout à gauche
-        partie.Serpent.Add(new Snake.CaseDeJeu(0,2)); 
-        partie.Serpent.Add(new Snake.CaseDeJeu(1,2));
-        partie.Serpent.Add(new Snake.CaseDeJeu(2,2));
-        
+        partie.Serpent.Add(new Snake.CaseDeJeu(0, 2));
+        partie.Serpent.Add(new Snake.CaseDeJeu(1, 2));
+        partie.Serpent.Add(new Snake.CaseDeJeu(2, 2));
+
         partie.PartieEnCours = true;
         partie.DirectionSerpent = Snake.Directions.Gauche; //va à gauche 
-        
+
         partie.PositionGateau = new Snake.CaseDeJeu(10, 5);
-        
+
         Snake.AvancerSerpent(ref partie);
-        
+
         Assert.IsFalse(partie.PartieEnCours);//cogne le bord donc le jeu s'arrête
     }
 
@@ -413,21 +450,21 @@ public class TestCalcul
     {
         Snake.Partie partie = new();
         partie.Serpent = new List<Snake.CaseDeJeu>();
-        
+
         //serpent en forme de C
-        partie.Serpent.Add(new Snake.CaseDeJeu(2, 2)); 
-        partie.Serpent.Add(new Snake.CaseDeJeu(3,2));
-        partie.Serpent.Add(new Snake.CaseDeJeu(3,3));
-        partie.Serpent.Add(new Snake.CaseDeJeu(2,3));//va cogner l'anneau qui se trouve à cette case car il va aller vers le bas
-        partie.Serpent.Add(new Snake.CaseDeJeu(1,3));  
-        
+        partie.Serpent.Add(new Snake.CaseDeJeu(2, 2));
+        partie.Serpent.Add(new Snake.CaseDeJeu(3, 2));
+        partie.Serpent.Add(new Snake.CaseDeJeu(3, 3));
+        partie.Serpent.Add(new Snake.CaseDeJeu(2, 3));//va cogner l'anneau qui se trouve à cette case car il va aller vers le bas
+        partie.Serpent.Add(new Snake.CaseDeJeu(1, 3));
+
         partie.PositionGateau = new Snake.CaseDeJeu(10, 5);
 
         partie.PartieEnCours = true;
-        partie.DirectionSerpent = Snake.Directions.Bas; 
-        
+        partie.DirectionSerpent = Snake.Directions.Bas;
+
         Snake.AvancerSerpent(ref partie);
-        
+
         Assert.IsFalse(partie.PartieEnCours);//colision donc le jeu s'arrête
     }
 
@@ -439,52 +476,52 @@ public class TestCalcul
 
         partie.Score = 4;
         partie.Vitesse = 25;
-        
-        partie.Serpent.Add(new Snake.CaseDeJeu(2, 2)); 
-        partie.Serpent.Add(new Snake.CaseDeJeu(3,2));
-        partie.Serpent.Add(new Snake.CaseDeJeu(3,3));
-        partie.Serpent.Add(new Snake.CaseDeJeu(2,3));
-        
+
+        partie.Serpent.Add(new Snake.CaseDeJeu(2, 2));
+        partie.Serpent.Add(new Snake.CaseDeJeu(3, 2));
+        partie.Serpent.Add(new Snake.CaseDeJeu(3, 3));
+        partie.Serpent.Add(new Snake.CaseDeJeu(2, 3));
+
         partie.PositionGateau = new Snake.CaseDeJeu(1, 2);//on place un gâteau sur la prochaine case 
-                                                       //où la tête du serpent va attérir car on le déplace vers la gauche
+                                                          //où la tête du serpent va attérir car on le déplace vers la gauche
         partie.PartieEnCours = true;
         partie.DirectionSerpent = Snake.Directions.Gauche;
-        
+
         Snake.AvancerSerpent(ref partie);
-        
-        Assert.AreEqual(5,partie.Score);//le score s'incrémente
-        Assert.AreEqual(Snake.VITESSE_MIN,partie.Vitesse);//la vitesse retombe à la vitesse minimal
-        Assert.AreEqual(5,partie.Serpent.Count);//1 anneau s'ajoute à la liste serpent 
+
+        Assert.AreEqual(5, partie.Score);//le score s'incrémente
+        Assert.AreEqual(Snake.VITESSE_MIN, partie.Vitesse);//la vitesse retombe à la vitesse minimal
+        Assert.AreEqual(5, partie.Serpent.Count);//1 anneau s'ajoute à la liste serpent 
         Assert.IsFalse(partie.EnleverQueue);//on enlève pas la queue car le serpent grandi 
-        
-        
+
+
     }
 
     [TestMethod]
     public void AvancerSerpentMangePasGateau()
     {
         Snake.Partie partie = new();
-        
+
         partie.Serpent = new List<Snake.CaseDeJeu>();
         partie.Score = 4;
         partie.Vitesse = 25;
-        
-        partie.Serpent.Add(new Snake.CaseDeJeu(2, 2)); 
-        partie.Serpent.Add(new Snake.CaseDeJeu(3,2));
-        partie.Serpent.Add(new Snake.CaseDeJeu(3,3));
-        partie.Serpent.Add(new Snake.CaseDeJeu(2,3));
-        
+
+        partie.Serpent.Add(new Snake.CaseDeJeu(2, 2));
+        partie.Serpent.Add(new Snake.CaseDeJeu(3, 2));
+        partie.Serpent.Add(new Snake.CaseDeJeu(3, 3));
+        partie.Serpent.Add(new Snake.CaseDeJeu(2, 3));
+
         partie.PositionGateau = new Snake.CaseDeJeu(10, 2);
-        
+
         partie.PartieEnCours = true;
         partie.DirectionSerpent = Snake.Directions.Haut;
-        
+
         Snake.AvancerSerpent(ref partie);
-        
-        Assert.AreEqual(26,partie.Vitesse);//vitesse s'incrémente
-        Assert.AreEqual(4,partie.Score);//score ne bouge pas
+
+        Assert.AreEqual(26, partie.Vitesse);//vitesse s'incrémente
+        Assert.AreEqual(4, partie.Score);//score ne bouge pas
         Assert.IsTrue(partie.EnleverQueue);//on enlève la queue pour que le serpent garde la meme taille
-                                            // vu qu'on ajoute une nouvelle tête à chaque fois qu'il avance 
+                                           // vu qu'on ajoute une nouvelle tête à chaque fois qu'il avance 
     }
 
 
